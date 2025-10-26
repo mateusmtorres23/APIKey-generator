872ad2af6298b44108f9c5157ad9299dd7524c5f
Mateus Torres <mmtorres230726@gmail.com>
2025-10-26 18:05:48 -0300

    creating the repo and adding the spring structure

---
+++ src/main/java/com/apikeygen/apikeygenerator/ApikeyGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.apikeygen.apikeygenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApikeyGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApikeyGeneratorApplication.class, args);
+	}
+
+}
